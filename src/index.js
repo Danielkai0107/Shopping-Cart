6b9b15dbcbc6dfc8e71bb0c695615666f0768276
Daniel <danielwzk0107@gmail.com>
2022-09-30 15:55:34 +0800

    fix json

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AppProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/Shopping-Cart">
         <Navbar />
         <Routes>
           <Route path="/" element={<ProductPage />} />
